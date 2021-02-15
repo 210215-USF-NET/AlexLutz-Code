f42e7e60ef1661404095c6e6f3f6a32d15ad449f
unknown <cg.alutz@hotmail.com>
2021-02-15 17:15:41 -0500

    Inserting helloworld stuff

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
